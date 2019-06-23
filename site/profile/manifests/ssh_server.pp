4bda81852790f2ee459113a16882b7ec03aa342f
flaggen <alex@dmtru.com>
2019-06-23 02:12:54 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC0wv8+VeE69EnU1it6X0WO8juXo/YGAhnkgr/A8s02axBZEGTQ4ajFndGL64UxAgu6sHPC9szJbTLTRJ5MSG8jvdBPsThKHKlvK4HHtqREw0N5TTa/jJVIYj4rO8svHSm4OKDttn6HwIJpsJhy2vFntKpErz8HQmN3EyIZQsPfRQRVq2lRT0fdfBIERP+Z9g3OvhBVG3lK07ZJN4hLO5PXl2ghIt85/q1wdnC1iFB/F0CQPgER6wVZrJ7CgehkjhoT0GT77UrqQ1RxHfEMVXx4ettdFpFDJ7S2AnF5PiovF4OcR+SsUivwW7FjAbgRprQQgg9vBJKhwVFwylqRLGlB',
   }  
 }
